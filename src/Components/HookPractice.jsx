48bf15a80a08742a19c70f6b19fbcfd53c4769aa
zocalop <jcb2861@gmail.com>
2026-05-11 17:21:02 -0300

    HookPractice updated in moor-hook-practice

--- src/Components/HookPractice.jsx
+++ src/Components/HookPractice.jsx
@@ -1,25 +1,27 @@
+
+
 import React, { useState, useMemo } from 'react';
 
 function ClickCounter() {
   const [clicks, setClicks] = useState(0);
-  const number = useMemo(() => {
-    return Math.floor(Math.random() * 10) + 1;
-  }, []);
-  const [buttonText, setButtonText] = useState();
-  const dice = useMemo(() => {
-    return setButtonText("Click Me");
-  }, []);
+  const handleClicks = () => {
+    setClicks(clicks + 1);
+  };
+  const [dice, setDice] = useState(Math.floor(Math.random() * 10) + 1);
+  const playAgain = () => {
+    setClicks(0);
+    setDice(Math.floor(Math.random() * 10) + 1);
+  };
 
   return (
     <div>
-      <p>You rolled a {number}</p>     
+      <p>You rolled a {dice}</p>     
 
-      <button onClick={() => setClicks(clicks + 1)}>
-        {buttonText}
+      <button onClick={clicks > dice ? playAgain : handleClicks}>
+        {clicks > dice ? "Play again" : " Click me"}
       </button>
 
-      {clicks > number ? (
-        setButtonText("Play again."),
+      {clicks > dice ? (
         <p>You died.
         Would you like to play again?</p>
         ) : (    
