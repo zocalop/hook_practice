908b9fa147d965ea9d99076c46f0f0b846bd7158
zocalop <jcb2861@gmail.com>
2026-05-11 23:55:31 -0300

    Updated HookPractic.jsx in master

--- src/Components/HookPractice.jsx
+++ src/Components/HookPractice.jsx
@@ -1,36 +1,57 @@
 
+ 
 
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 
 function ClickCounter() {
+  const [diceFaces, setDiceFaces] = useState(10);
   const [clicks, setClicks] = useState(0);
+  const [placeholder, setPlaceholder] = useState(`Your die has ${diceFaces} faces.`);
+  const [value, setValue] = useState();
+  const random = Math.floor(Math.random() * diceFaces) + 1;
   const handleClicks = () => {
     setClicks(clicks + 1);
   };
-  const [dice, setDice] = useState(Math.floor(Math.random() * 10) + 1);
+  const [roll, setRoll] = useState(random);
   const playAgain = () => {
     setClicks(0);
-    setDice(Math.floor(Math.random() * 10) + 1);
+    setRoll(random);
   };
+  const changeHandler = () => {
+    setClicks(0);
+    setRoll(random);
+    setPlaceholder(`Your die has ${diceFaces} faces.`);
+    setDiceFaces(value);
+  };  
 
   return (
     <div>
-      <p>You rolled a {dice}</p>     
-
-      <button onClick={clicks > dice ? playAgain : handleClicks}>
-        {clicks > dice ? "Play again" : " Click me"}
+      <p>You rolled a {roll}</p>
+      <button onClick={clicks > roll ? playAgain : handleClicks}>
+        {clicks > roll ? "Play again" : " Click me"}
       </button>
 
-      {clicks > dice ? (
+      {clicks > roll ? (
         <p>You died.
         Would you like to play again?</p>
         ) : (    
-        <p>You clicked {clicks} many times.</p>
-      )}
-
+        <p>You clicked {clicks} times.</p>
+      )}   
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+	  placeholder={placeholder}
+        />
+        <div></div>
+        <button type="button" onClick={changeHandler}>
+          Change Dice
+        </button>
+      </form>
     </div>
-  );
-}
+  )
+};
 
 export default ClickCounter;
+
 
